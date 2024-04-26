1141847d8031219f70abc9ceb8016252f89d3ad2
Jini <jini@jinis-air.speedport.ip>
2024-04-26 14:45:16 +0200

    edit files

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# define the dotfiles
+zsh_dotfiles=(
+	.zshenv
+	.zshrc
+)
+# make a symbolic link to the home directory
+for file in "${zsh_dotfiles[@]}"; do
+	ln -svf ~/dotfiles/zsh/$file ~
+done
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/dotfiles/zsh/.zprezto
+
+# set .vim
+ln -svf ~/dotfiles/vim/.vimrc ~
+ln -svf ~/dotfiles/vim/.gvimrc ~
+mkdir ~/.vim
+mkdir ~/.vim/colors
+ln -svf ~/dotfiles/vim/colors/iceburg.vim ~/.vim/colors
+
+# set atcoder-tools
+mkdir ~/.atcoder-tools
+ln -svf ~/dotfiles/atcoder-tools/.atcodertools.toml ~/.atcoder-tools
+ln -svf ~/dotfiles/atcoder-tools/my_template.cpp ~/.atcoder-tools
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -5,10 +5,11 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-export ZDOTDIR=$HOME/dotfiles/.zsh
+export ZDOTDIR=$HOME/dotfiles/zsh
 
 # Ensure that a non-login, non-interactive shell has a defined environment.
 if [[ ( "$SHLVL" -eq 1 && ! -o LOGIN ) && -s "${ZDOTDIR:-$HOME}/.zprofile" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprofile"
 fi
+
 . "$HOME/.cargo/env"
